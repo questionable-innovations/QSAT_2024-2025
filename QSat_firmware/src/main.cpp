eb207e4b490d3ad4bb0f5df30fadb86c63950dce
Jasper Miller-Waugh <contact@jmw.nz>
2025-02-25 17:28:41 +1300

    firmware base

---
+++ QSat_firmware/src/main.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+#include <Energia.h>
+
+long unsigned int currentValue = 0;
+
+long resolution = 255;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(P2_0, OUTPUT);
+  analogFrequency(50);
+  analogResolution(resolution);
+}
+
+int old_duty = 0;
+
+void angle_set(int pin, int angle) {
+  angle = max(0, min(180, angle));
+  float active_time = 1.0 / 20.0 + (angle / 180.0) * 1.0 / 20.0;
+  // float active_time = (angle / 180.0);
+  int duty = (int)(active_time * resolution);
+
+  if (duty == old_duty) {
+    return;
+  }
+  old_duty = duty;
+  analogWrite(pin, (int)(duty));
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int angle = currentValue % 180;
+  Serial.print("Angle: ");
+  // Serial.print(digitalPinToTimer(P2_0));
+  Serial.println(angle);
+  // angle_set(P1_6, angle);
+  angle_set(P2_0, angle);
+  
+  currentValue += 1;
+  delay(10);
+}
