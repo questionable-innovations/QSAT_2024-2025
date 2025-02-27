ae9101b0f3c927c8a9598f492107afb3ce68e4d1
Ross Porter <rossporter506@gmail.com>
2025-02-28 00:23:18 +1300

    Simplify panic handler
    Remove serial configuration fn. The mere presence of clock configuration caused freezes after initial clock configuration, even when panics don't occur(??)
    It's kind of silly to duplicate all the code to try and set up serial considering it happens basically first-thing before any panics can occur, at least as the code is written now.

--- PSat/MCU/Firmware/Rust/src/panic_handler.rs
+++ PSat/MCU/Firmware/Rust/src/panic_handler.rs
@@ -1,5 +1,3 @@
-use msp430fr2x5x_hal::{clock::{DcoclkFreqSel, MclkDiv, SmclkDiv}, gpio::Batch, pmm::Pmm};
-
 // Our panic handler. Currently we print strings here for maximum debuggability. String printing is quite expensive in terms of executable size,
 // so if you're running out of space consider commenting out some of these print statements (or uncommenting `strip = true` in cargo.toml!).
 use crate::{print, println, stdlib_println};
@@ -8,36 +6,22 @@ use core::panic::PanicInfo;
 fn panic_handler(panic_info: &PanicInfo) -> ! {
     msp430::interrupt::disable();
 
-    //ensure_serial_configured();
-    print!("Panic: ");
-    if let Some(location) = panic_info.location() {
-        // Printing code locations adds a lot of executable size
-        println!("File: {}, line: {}, col: {},", location.file(), location.line(), location.column())
+    let serial_configured = msp430::critical_section::with(|cs| { crate::serial::SERIAL.borrow_ref(cs).is_some() });
+    if serial_configured {
+        print!("Panic: ");
+        if let Some(location) = panic_info.location() {
+            // Printing code locations adds a lot of executable size
+            println!("File: {}, line: {}, col: {},", location.file(), location.line(), location.column())
+        }
+        if let Some(message) = panic_info.message().as_str() {
+            print!("{}", message);
+        }
+        else {
+            // Unfortunately we can't print PanicMessage using ufmt because it doesn't implement uDisplay or uDebug.
+            // The below code pulls in Rust's standard printing library. This takes more executable space, remove it if you need to.
+            stdlib_println!("{}", panic_info.message());
+            //println!("Can't print message");
+        }
     }
-    if let Some(message) = panic_info.message().as_str() {
-        print!("{}", message);
-    }
-    else {
-        // Unfortunately we can't print PanicMessage using ufmt because it doesn't implement uDisplay or uDebug.
-        // The below code pulls in Rust's standard printing library.
-        stdlib_println!("{}", panic_info.message());
-        //println!("Can't print message");
-    }
-
     loop { msp430::asm::barrier(); }
-}
-
-// *Ensure* debug serial is configured, stealing registers and pins if necessary.
-fn ensure_serial_configured() {
-    let not_configured = msp430::critical_section::with(|cs| { crate::serial::SERIAL.borrow_ref(cs).is_none() });
-    if not_configured {
-        let regs = unsafe{ msp430fr2x5x_hal::pac::Peripherals::steal() };
-        let pin1_7: crate::pin_mappings::DebugTxPin = Batch::new(regs.P1).split(&Pmm::new(regs.PMM)).pin7.to_alternate1();
-        let mut fram = msp430fr2x5x_hal::fram::Fram::new(regs.FRCTL);
-        let (smclk, _aclk, _delay) = msp430fr2x5x_hal::clock::ClockConfig::new(regs.CS)
-            .mclk_dcoclk(DcoclkFreqSel::_8MHz, MclkDiv::_1)
-            .smclk_on(SmclkDiv::_1)
-            .freeze(&mut fram);
-        crate::serial::configure_debug_serial(pin1_7, &smclk, regs.E_USCI_A0);
-    }
 }
