5338418bdba8d0d2d35c84b2b2f9ecfa8a258972
Jasper M-W <contact@jmw.nz>
2025-03-05 20:23:04 +1300

    Power controls

--- QSat_firmware_2/src/main.cpp
+++ QSat_firmware_2/src/main.cpp
@@ -3,28 +3,20 @@
 #include "shutter.h"
 
 
-
-
-
-// FUNCTION DEFINITIONS
-bool detectLight(); 
-void fireShutter(); // only activate if no pings for 10 seconds
-
-
-
 // SETUP 
 void setup() {
   Serial.begin(115200); // forgor serial baud rate maybe that's right lmao
   Serial.println("Starting up");
   setup_shutter();
   setup_radio();
-
 }
 
 
 // LOOP
 void loop() {
-  Serial.println("e :)");
-
+  fire_shutter();
+  loop_shutter();
+  
+  
   loop_radio();
 }
--- QSat_firmware_2/src/shutter.cpp
+++ QSat_firmware_2/src/shutter.cpp
@@ -2,8 +2,8 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
 
-int ServoPin = 13;
-ESP32PWM pwm;
+int ServoPin = 17;
+// ESP32PWM pwm;
 int freq = 1000;
 
 
@@ -15,25 +15,54 @@ int freq = 1000;
 long int last_fired_time = 0;
 uint8_t ldrThreshold = 128;
 
+bool high_power_enabled = false;
+
+int high_power_timeout = 30000; // 30 seconds
+
 void setup_shutter() {
+    Serial.println("Setting up shutter");
     pinMode(LDR_PIN, INPUT);
     pinMode(PWR_ENABLE, OUTPUT);
     pinMode(SHUTTER_PIN, OUTPUT);
 
-    ESP32PWM::allocateTimer(0);
-	pwm.attachPin(ServoPin, freq, 10); // 1KHz 10 bits
+    digitalWrite(PWR_ENABLE, LOW);
+
+    // ESP32PWM::allocateTimer(0);
+	// pwm.attachPin(ServoPin, freq, 10); // 1KHz 10 bits
 
 }
 
 void loop_shutter() {
+    if (high_power_enabled && millis() - last_fired_time > high_power_timeout) {
+        // Go back to sleep
+        digitalWrite(PWR_ENABLE, LOW);
+        high_power_enabled = false;
+    }
+}
+
+void enable_high_power() {
+    Serial.println("Enabling high power");
+
+    digitalWrite(PWR_ENABLE, HIGH);
+    high_power_enabled = true;
+    delay(100);
 }
 
 void fire_shutter() {
-    pwm.writeScaled(0);
+    enable_high_power();
+
+    Serial.println("Firing Shutter");
+    analogWrite(SHUTTER_PIN, (2*255)/20);
     delay(1000);
-    pwm.writeScaled(1);
+    analogWrite(SHUTTER_PIN, (1*255)/20);
+    // pwm.write(180);
     delay(1000);
-    pwm.writeScaled(0);
+
+    // pwm.write(0);
+    // delay(1000);
+    // pwm.write(180);
+    // delay(1000);
+    // pwm.write(0);
     last_fired_time = millis();
 }
 
