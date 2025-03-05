580a2ba76bcf5872f90dc5a88e70ae9d83a92257
Taine Reader <taine.reader@gmail.com>
2025-03-06 01:53:55 +1300

    gift i'm sleeping

--- ESPCAM/src/main.cpp
+++ ESPCAM/src/main.cpp
@@ -88,49 +88,48 @@ void setup() {
     Serial.println("No SD Card attached");
     return;
   }
+
+  // force flash off
+  // pinMode(4, OUTPUT);
+  // digitalWrite(4, LOW);
+  // rtc_gpio_hold_en(GPIO_NUM_4);
+  
+  while (true) {
+    camera_fb_t * fb = NULL;
     
-  camera_fb_t * fb = NULL;
-  
-  // Take Picture with Camera
-  fb = esp_camera_fb_get();  
-  if(!fb) {
-    Serial.println("Camera capture failed");
-    return;
-  }
-  // initialize EEPROM with predefined size
-  EEPROM.begin(EEPROM_SIZE);
-  pictureNumber = EEPROM.read(0) + 1;
+    // Take Picture with Camera
+    fb = esp_camera_fb_get();  
+    if(!fb) {
+      Serial.println("Camera capture failed");
+      return;
+    }
+    // initialize EEPROM with predefined size
+    EEPROM.begin(EEPROM_SIZE);
+    pictureNumber = EEPROM.read(0) + 1;
 
-  // Path where new picture will be saved in SD Card
-  String path = "/picture" + String(pictureNumber) +".jpg";
-  // String path = "/picture" + String(millis()) + ".jpg";
+    // Path where new picture will be saved in SD Card
+    String path = "/picture" + String(pictureNumber) +".jpg";
+    // String path = "/picture" + String(millis()) + ".jpg";
 
-  fs::FS &fs = SD_MMC; 
-  Serial.printf("Picture file name: %s\n", path.c_str());
-  
-  File file = fs.open(path.c_str(), FILE_WRITE);
-  if(!file){
-    Serial.println("Failed to open file in writing mode");
-  } 
-  else {
-    file.write(fb->buf, fb->len); // payload (image), payload length
-    Serial.printf("Saved file to path: %s\n", path.c_str());
-    EEPROM.write(0, pictureNumber);
-    EEPROM.commit();
+    fs::FS &fs = SD_MMC; 
+    Serial.printf("Picture file name: %s\n", path.c_str());
+    
+    File file = fs.open(path.c_str(), FILE_WRITE);
+    if(!file){
+      Serial.println("Failed to open file in writing mode");
+    } 
+    else {
+      file.write(fb->buf, fb->len); // payload (image), payload length
+      Serial.printf("Saved file to path: %s\n", path.c_str());
+      EEPROM.write(0, pictureNumber);
+      EEPROM.commit();
+    }
+    file.close();
+    esp_camera_fb_return(fb); 
+
+    delay(1000); // Add a delay to avoid taking pictures too quickly
   }
-  file.close();
-  esp_camera_fb_return(fb); 
   
-  // Turns off the ESP32-CAM white on-board LED (flash) connected to GPIO 4
-  pinMode(4, OUTPUT);
-  digitalWrite(4, LOW);
-  rtc_gpio_hold_en(GPIO_NUM_4);
-  
-  delay(2000);
-  Serial.println("Going to sleep now");
-  delay(2000);
-  esp_deep_sleep_start();
-  Serial.println("This will never be printed");
 }
 
 void loop() {
--- QSat_firmware_2/src/radio.cpp
+++ QSat_firmware_2/src/radio.cpp
@@ -4,7 +4,7 @@
 
 #include "ESPNowW.h"
 
-#ifdef sender
+#ifdef rocket
 uint8_t mac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33};
 uint8_t receiver_mac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x32};
 #else
@@ -14,6 +14,8 @@ uint8_t receiver_mac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33};
 
 unsigned long last_ping_sent = 0;
 unsigned long last_ping_received = 0;
+
+uint8_t isArmed = 0;
 
 unsigned long disconnectTime = 10000;
 
@@ -32,8 +34,7 @@ void setup_radio() {
 void send_ping() {
     Serial.println("Sending ping");
     // send ping
-    uint8_t ping = 1;
-    ESPNow.send_message(receiver_mac, &ping, 1);
+    ESPNow.send_message(receiver_mac, &isArmed, 1);  // 1 for armed, 0 for disarmed
 }
 
 void loop_radio() {
@@ -43,8 +44,9 @@ void loop_radio() {
     }
 
     if (last_ping_received > millis() + disconnectTime) {
-        // no connection probably, explode
+        // no connection probably, arm
         Serial.println("No connection, exploding");
+        isArmed = 1;
     }
 }
 
@@ -66,6 +68,14 @@ void onPingReceived(const uint8_t *mac_addr, const uint8_t *data, int data_len) 
     }
     Serial.println("");
     // bs finish
+
+    // set isArmed
+    isArmed = data[0];
+    if (isArmed) {
+        Serial.println("RECIEVED 1: System is armed.");
+    } else {
+        Serial.println("DID NOT RECIEVE 1: System is disarmed.");
+    }
 
     last_ping_received = millis();
 }
